a06b2455642609744bc9010d2b3e0d0c8f9e4177
Juauum <joaopedrohh2018@gmail.com>
2024-07-26 20:57:01 -0300

    commit

---
+++ pagina inicial/pag_inicial.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Pagina Inicial</title>
+</head>
+<body>
+  <!---------HEADER------------->
+ <header>
+    <div class="div1">
+      <img src="#" alt="logo">
+      <p>Professor</p>
+      <p>xxxxxxxxx</p>
+    </div>
+    <div class="div2">
+      <form action="#" method="get">
+        <input type="text" name="texto" id="texto">
+      </form>
+      <img src="#" alt="imagem">
+    </div>
+  </header>
+ <!------------------------------>
+ 
+ <!-----------NAV---------------->
+ <nav>
+    <h2>Alguma das vagas</h2>
+
+  </nav>
+<!------------------------------>
+
+<!--------------MAIN------------>
+<main>
+  <!-------SECAO-01------------->
+  <section class="secao1">
+    <h2>Resumo do "sobre"/ propósito do site</h2>
+    <div>
+      <img src="#" alt="imagem">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cupiditate reprehenderit mollitia quis deserunt. Esse accusantium nulla animi aspernatur repellat molestiae? Maxime laborum facere corrupti ducimus autem enim soluta inventore!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod aliquam odit. Ipsa vero deleniti, reiciendis fugit temporibus quod iure numquam quae sed tenetur doloribus tempore dignissimos eius sunt officia!
+        Commodi rerum dolorum, magnam assumenda voluptate ut delectus dolorem voluptatibus vel? Assumenda porro dignissimos hic error inventore adipisci quos accusamus nostrum, velit in laborum quasi laudantium ex quo ducimus tenetur?
+      </p>
+    </div>
+  </section>
+  <!--------------------------->
+
+  <!--------SECAO-02----------->
+  <section class="secao2">
+    <h2>Estrutura interativa com instruções de uso inicial do site</h2>
+    <h3>Veja como funciona:</h3>
+    <ol>
+      <li>lorem Ipsum <p>optio dolor consert</p> </li>
+      <li>Lorem Ipsum<p>optio dolor consert</p></li>
+      <li>Lorem Ipsum<p>optio dolor consert</p></li>  
+    </ol>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In repellat dolore animi at ex dolor veniam officia ullam. Qui, iure aperiam fugit deserunt amet laborum vero voluptatem porro culpa itaque.
+    Suscipit ullam explicabo amet corrupti dignissimos ut, incidunt sapiente ipsum provident, velit vitae itaque earum, eos fugiat consequatur labore. Alias non ea neque quasi dicta tenetur dolore totam fugit sapiente?</p>
+  </section>
+  <!------------------------------>
+
+  <!-----------SECAO-03----------->
+  <section class="secao3">
+      <img src="#" alt="imagem">
+      <div>
+        <h2>Lorem ipsum, dolor sit amet</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam reiciendis voluptas rem at architecto dolores odit magni cum recusandae temporibus, illum corrupti, cupiditate voluptates quod. Inventore, minus commodi. Repudiandae, qui.
+          Totam at qui nihil asperiores dolor provident incidunt excepturi sapiente hic nesciunt debitis aliquid numquam aperiam repellendus vel, possimus odio eveniet. Ab itaque perferendis aperiam qui quibusdam temporibus magnam sint?
+          Rerum, tempore molestias. Quae laboriosam illo temporibus, debitis fuga ipsa reiciendis in soluta ducimus hic porro ipsam saepe nesciunt at tempora. Necessitatibus id, repellendus nam aliquam nulla suscipit! Laboriosam, ab.
+        </p>
+      </div>   
+  </section>
+  <!------------------------------>
+
+  <!----------SECAO-04------------>
+  <section class="secao4">
+    <div>
+      <h2>Lorem ipsum, dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quibusdam, velit maiores magni ea vel fuga itaque fugiat fugit voluptatem, dolore, aperiam amet non molestias atque ad minus voluptate adipisci?
+        Ea laboriosam illum, beatae quasi voluptatem rem facilis cupiditate amet? Ducimus eligendi nihil temporibus magnam possimus illo maiores labore iusto porro totam modi alias repudiandae dolore, fuga voluptatibus quaerat. Optio.
+        Tempore sit vel ipsum natus nostrum quidem veniam illo! Velit iure inventore ratione possimus sapiente? Necessitatibus quibusdam quos voluptatum debitis, voluptates accusantium qui deleniti error magnam odio ratione, blanditiis perferendis?
+      </p>
+    </div>
+    <img src="#" alt="imagem">
+  </section>
+</main>
+<!------------------------------>  
+
+<!------------FOOTER------------>
+  <footer>
+    <div>
+      <p>Para candidatos</p>
+      <p>cadastrar curriculo</p>
+    </div>
+    <div>
+      <p>Para empresas</p>
+      <p>Anunciar vaga</p>
+    </div>
+    <div>
+      <p>Institucional</p>
+      <p>Ajuda</p>
+      <p>Sobre nós</p>
+    </div>
+    <div>
+      <p>Redes Sociais</p>
+      <p><img src="#" alt="ico"><img src="#" alt="ico"><img src="#" alt="ico"></p>
+    </div>
+  </footer>
+  <!------------------------------>
+  
+</body>
+</html>
